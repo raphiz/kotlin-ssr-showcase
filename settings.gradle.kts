b1ff3489160971395282c21d9a46d44ed9e89ed5
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2024-07-17 14:54:35 +0200

    build: Remove unused develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,14 +12,3 @@ dependencyResolutionManagement {
     }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
-
-plugins {
-    id("com.gradle.develocity") version "3.17"
-}
-
-develocity {
-    buildScan {
-        termsOfUseUrl = "https://gradle.com/terms-of-service"
-        termsOfUseAgree = "yes"
-    }
-}
