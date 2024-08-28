23b8adacf126f12185ea22d4bfc62d0f6d2e21b8
Raphael Zimmermann <mail@raphael.li>
2024-08-28 16:59:05 +0200

    build: Add buildSrc project

---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
